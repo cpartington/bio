8af72ea20ac60a9312256041196c39e88c16118d
Christie Partington <christiedpartington@gmail.com>
2018-10-08 21:53:22 -0600

    Reformatted package.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name="bio-info",
+setup(name="bio_info",
       version="0.1",
       description="Basic algorithms for DNA manipulation.",
       url="http://github.com/cpartington/bio-info",
       author="Christie Partington",
-      packages=["bio-info"],
+      packages=["bio_info"],
       install_requires=[
           "progressbar2"
       ])
