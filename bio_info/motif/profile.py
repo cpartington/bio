dac5edf003d623a07925f7853a96397717699692
Christie Partington <christiedpartington@gmail.com>
2018-10-13 13:23:08 -0600

    Form profile from various structures

--- bio_info/motif/profile.py
+++ bio_info/motif/profile.py
@@ -4,9 +4,15 @@ from ..general import *
 class Profile:
 
     def __init__(self, motif_list):
-        self.profile = self.make(motif_list)
+        val = motif_list[0][0]
+        if type(val) == str:
+            self.profile = self.from_dna_list(motif_list)
+        elif type(val) == int:
+            self.profile = self.from_count(motif_list)
+        elif type(val) == float:
+            self.profile = self.from_profile(motif_list)
 
-    def make(self, motif_list):
+    def from_dna_list(self, motif_list):
         """
         Get profile matrix for a list of motifs.
         """
@@ -26,6 +32,28 @@ class Profile:
             profile[2].append((g_num + 1) / t)
             profile[3].append((t_num + 1) / t)
 
+        return profile
+
+    def from_count(self, count_matrix):
+        k = len(count_matrix[0])
+        profile = list()
+        profile.append(list())
+        profile.append(list())
+        profile.append(list())
+        profile.append(list())
+
+        for i in range(k):
+            col = [count[i] for count in count_matrix]
+            t = sum([col[0], col[1], col[2], col[3]]) + 4
+            profile[0].append((col[0] + 1) / t)
+            profile[1].append((col[1] + 1) / t)
+            profile[2].append((col[2] + 1) / t)
+            profile[3].append((col[3] + 1) / t)
+
+        return profile
+
+
+    def from_profile(self, profile):
         return profile
 
     def get(self, row=None, col=None):
