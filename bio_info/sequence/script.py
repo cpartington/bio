3d4ee533efa0f70c4aa04afb1c423b38fbf06b51
Christie Partington <christiedpartington@gmail.com>
2018-11-03 16:12:32 -0600

    Added reconstruct_from_pairs

--- bio_info/sequence/script.py
+++ bio_info/sequence/script.py
@@ -22,3 +22,23 @@ def reconstruct(dna_list):
         seq_list.append(node.label[0])
     seq_list.append(path[-1].label[1:])
     return "".join(seq_list)
+
+
+def reconstruct_from_pairs(dna_list, k, d, sep=','):
+    sub_k = k - 1
+    sub_d = d + 1
+
+    print("Building graph...")
+    g = de_bruijn(dna_list, from_pairs=True, sep=sep)
+
+    print("Finding path...")
+    path = eulerian_path(g)
+
+    print("Building sequence")
+    seq = list()
+    for node in path:
+        seq.append(node.label[0])
+    for node in path[-(sub_k + sub_d):-1]:
+        seq.append(node.label.split(',')[1][0])
+    seq.append(path[-1].label.split(',')[1])
+    return "".join(seq)
